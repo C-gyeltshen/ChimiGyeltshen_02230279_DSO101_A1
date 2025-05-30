ad6681b33725d840a54614e8cb68ef5f7ea3f294
C-gyeltshen <02230279.cst@rub.edu.bt>
2025-05-30 20:48:59 +0600

    wip test

--- frontend/__tests__/page.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import ToDo from '../src/app/todo/page' // <-- corrected path
-
-test('renders a heading', () => {
-  render(<ToDo />)
-  const heading = screen.getByRole('heading', { level: 1 })
-  expect(heading).toBeInTheDocument()
-})
--- frontend/__tests__/snapshot.js
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react'
-import ToDo from '../src/app/todo/page' // <-- corrected path
-
-test('renders the ToDo component correctly', () => {
-  const { container } = render(<ToDo />)
-  expect(container).toMatchSnapshot()
-})
